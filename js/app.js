431b12eeb780b6e3a9042ab5abfbbf3b20fad43a
iamkamal-cheema <kamal.cheema53@gmail.com>
2015-10-15 16:57:52 -0400

    fix: Remove Intial allEnemies Error

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x,y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y= y;
+
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
+
+var enemy= new Enemy();
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -24,11 +29,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
 
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function (dt) {
+
+};
+
+Player.prototype.render = function () {
+
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+  var player= new Player();
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies=[enemy];
 
 
 
